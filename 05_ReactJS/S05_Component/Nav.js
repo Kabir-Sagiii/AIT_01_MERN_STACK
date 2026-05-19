a21f6b4bafdd3a7d074d85c19c7af08189621742
Kabir <sagithedeveloper@gmail.com>
2026-05-19 07:54:59 +0530

    Added

---
+++ 05_ReactJS/S05_Component/Nav.js
@@ -0,0 +1,19 @@
+function Nav() {
+  return (
+    <div id="nav">
+      <div>
+        <h1>BetterWorkTeam</h1>
+      </div>
+      <div>
+        <a href="">Home</a>
+        <a href="">Profile</a>
+        <a href="">Products</a>
+        <a href="">Contactus</a>
+        <a href="">Cart</a>
+        <button> Logout </button>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ 05_ReactJS/S07_Components_Props/src/components/home/Home.jsx
@@ -0,0 +1,18 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="main">
+      <h1>Explore, The Site for More Information</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam
+        facilis eligendi, dolorum repellat iste ea deserunt magnam veritatis,
+        exercitationem tempora incidunt aliquam ut libero fugit excepturi
+        aspernatur ipsa earum blanditiis odio nesciunt necessitatibus! Numquam,
+        quisquam, officia quo distinctio
+      </p>
+    </div>
+  );
+}
+
+export default Home;
