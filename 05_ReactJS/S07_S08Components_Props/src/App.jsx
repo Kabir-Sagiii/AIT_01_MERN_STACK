577ca4c5a7cc03ddc793becc20d166ca57f16a44
Kabir <sagithedeveloper@gmail.com>
2026-05-22 07:56:10 +0530

    Added

--- 05_ReactJS/S07_S08Components_Props/src/App.jsx
+++ 05_ReactJS/S07_S08Components_Props/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import Home from "./components/home/Home.jsx";
+import CardContainer from "./components/card-container/CardContainer.jsx";
 
 function App() {
   return (
     <div>
       <Header />
       <Home />
+      <CardContainer />
       <Footer />
     </div>
   );
